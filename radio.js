1bbf4362103b15cccf41bd09251e85986b5e1b5c
luvdjsilvah <luvdjsilvah@gmail.com>
2026-09-14 23:50:32 -0700

    Update radio.js

--- radio.js
+++ radio.js
@@ -1201,7 +1201,7 @@ const playlist = [
       {
         width: "100%",
         height: "100%",
-        objectFit: "cover",
+     objectFit: "contain",
         display: "none",
         background: "#000"
       }
